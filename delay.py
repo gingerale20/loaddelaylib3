dcd4ea9a9a950dc373f57a3a931bddecc08038e9
gingerale20 <159393754+gingerale20@users.noreply.github.com>
2024-04-19 23:07:25 -0400

    Update delay.py

--- delay.py
+++ delay.py
@@ -8,13 +8,19 @@ import sys
 #|____/\____(____  /\____ \____ |\___  >____(____  / ____||____/__||___  /______  /
 #                \/      \/    \/    \/          \/\/                  \/       \/ 
 
-def print_d(words, delay=""):
-    d2 = float(delay)
-    for char in words:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(d2)
-    sys.stdout.write("""
+class delay:
+    def print(words, delay):
+        d2 = float(delay)
+        for char in words:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(d2)
+        sys.stdout.write("""
 """)
-    sys.stdout.flush()
-
+        sys.stdout.flush()
+    def in_pr(words, delay):
+        d2 = float(delay)
+        for char in words:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(d2)
